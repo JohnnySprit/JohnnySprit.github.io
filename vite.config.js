dfe88d883f4d473f94aeb6d0af726dec9a14df83
JohnnySprit <flanegt@gmail.com>
2025-04-05 00:24:42 -0400

    added predeploy and deploy in package.json

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/JohnnySprit.github.io",
   server: {
     port: 8000,
     watch: {
